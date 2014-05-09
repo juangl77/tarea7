599cbd231d30fce69497c2632e397388a74f6158
JuanDavid <juampis77@hotmail.com>
2014-05-09 00:44:18 -0500

    Se daña en la pared

--- punto2/schrodinger.c
+++ punto2/schrodinger.c
@@ -12,17 +12,18 @@ int main(){
   float *v = malloc(n_points*sizeof(float));
   float *i_initial = malloc(n_points*sizeof(float));
   float *r_initial = malloc(n_points*sizeof(float));
-  int n_time = 1000; 
+  int n_time = 40000; 
+  float L = 10;
   int i;
   for(i = 0;i<n_points;i++){
-    x[i] = 10/((float)n_points-1)*i;
+    x[i] = L/((float)n_points-1)*i;
     i_initial[i] = exp(-0.5*pow(((x[i]-5.0)/sigma),2))*cos(k*x[i]);
     r_initial[i] = exp(-0.5*pow(((x[i]-5.0)/sigma),2))*sin(k*x[i]);
     v[i] = x[i]*x[i]/2;
   }
   //Primera iteración para fijar condiciones de frontera
   float delta_x = x[1]-x[0];
-  float delta_t = 0.00005;
+  float delta_t = 0.000005;
   
   
   //Fijando condiciones iniciales.
@@ -34,44 +35,43 @@ int main(){
 
   //Creando array para todos los tiempos
  
-  float *r = malloc(n_points*n_time*sizeof(float));
-  float *im = malloc(n_points*n_time*sizeof(float));
+  float *r = malloc(n_points*sizeof(float));
+  float *im = malloc(n_points*sizeof(float));
   float *p = malloc(n_points*n_time*sizeof(float));
   int j;
-  //Asignando condiciones iniciales
-  for(i = 0; i < n_points;i++){
-    r[i] = r_initial[i];
-    im[i] = i_initial[i];
-    p[i] = pow(r_initial[i],2) +  pow(i_initial[i],2);
-  }
-  for(j = 0; j < n_time; j++){
-    r[n_points*j]=0;
-    r[n_points*j+n_points-1] = 0;
-    im[n_points*j]=0;
-    im[n_points*j+n_points-1] = 0;
-    p[n_points*j]=0;
-    p[n_points*j+n_points-1] = 0;
-  }
-  //Integrando
-  float alpha = delta_t/(delta_x*delta_x);
-  for(j = 1;j < n_time; j++){
-    for( i = 1; i < n_points-1;i++){
-      r[n_points*j+i] = r[n_points*(j-1)+i] - (alpha*(im[n_points*(j-1)+i+1]+im[n_points*(j+1)+i-1])-4*(alpha/2+v[i]*delta_t)*im[n_points*(j-1)+i]);
-    
-      im[n_points*j+i] = im[n_points*(j-1)+i] + (alpha*(r[n_points*j+i+1]+r[n_points*j+i-1])-4*(alpha/2+v[i]*delta_t)*r[n_points*j+i]);;
-      p[n_points*j+i] = r[n_points*j+i]*r[n_points*(j-1)+i] +  pow(im[n_points*j+i],2);
-     
-    }
+  //Asignando condiciones frontera
+    r[0]=0;
+    r[n_points-1] = 0;
+    im[0]=0;
+    im[n_points-1] = 0;
   
+  //Integrando
+  float alpha = delta_t/(2*delta_x*delta_x);
+  for(j = 0;j < n_time; j++){
+    for(i = 1; i < n_points-1;i++){
+      r[i] = r_initial[i] - 2*(alpha*(i_initial[i+1]+i_initial[i-1])-2*(alpha+delta_t*v[i])*i_initial[i]);
+    }
+    for(i = 0; i < n_points-1;i++){
+      p[n_points*j + i] = pow( i_initial[i],2) +  r[i]*r_initial[i];
+    }
+    for(i = 1; i < n_points-1;i++){
+      im[i] = i_initial[i] + 2*(alpha*(r[i+1]+r[i-1])-2*(alpha+delta_t*v[i])*r[i]);
+    }
+    for(i = 0; i < n_points;i++){
+      r_initial[i] = r[i];
+      i_initial[i] = im[i];
+    }
+   
   }
   //Imprimiendo resultados
   FILE *in;
   in = fopen("resultados_schro.txt","w");
-  for(i=0; i < n_time; i++){
+  for(i=0; i < n_time; i = i+10){
     for(j = 0; j < n_points; j++){
       fprintf(in, "%f ", p[n_points*i+j]);
     }
     fprintf(in,"\n");
   }
+  fclose(in);
   return 0;
 }
